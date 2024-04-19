a023801447657d8b16da87a3064dd33dd9be42ce
Kshitijell <kshitij.thakur@ellicium.com>
2024-04-19 16:56:26 +0530

    -ip chnage again

--- src/auth/context/msal/auth-provider.jsx
+++ src/auth/context/msal/auth-provider.jsx
@@ -90,7 +90,7 @@ export function AuthProvider({ children }) {
       formData.append('username', res.username);
       formData.append('password', res.password);
 
-      const response = await fetch('http://172.31.16.232:5000/login', {
+      const response = await fetch('http://52.1.28.231:5000:5000/login', {
         method: 'POST',
         body: formData,
       });
